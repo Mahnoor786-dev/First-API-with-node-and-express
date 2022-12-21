5d4c16d8290ecec00fdc0e2113a05b3656d13fcc
Mahnoor <mahnoorali57se@gmail.com>
2022-12-22 00:27:05 +0500

    DELETE ENDPOINT

--- src/controllers/crmController.js
+++ src/controllers/crmController.js
@@ -41,3 +41,12 @@ export const updateContact = (req, res) => {
     });
 };
 
+export const deleteContact = (req, res) => {
+    Contact.remove({ _id: req.params.contactId }, (err) => {
+        if (err) {
+            res.send(err);
+        }
+        res.json({ message: 'Successfully deleted contact'});
+    });
+};
+
--- src/routes/crmRoute.js
+++ src/routes/crmRoute.js
@@ -1,28 +1,31 @@
-import { addNewContact, getContacts, getContactWithID, updateContact } from '../controllers/crmController';
+import { addNewContact, 
+    getContacts, 
+    getContactWithID, 
+    updateContact, 
+    deleteContact } from '../controllers/crmController';
 
 const routes = (app) => {
-    app.route('/contact')
-    // get all contacts
-    .get((req,res, next) => {
-        // middleware
-        console.log(`Request from: ${req.originalUrl}`)
-        console.log(`Request type: ${req.method}`)
-        next();  
-    }, getContacts)
+app.route('/contact')
+// get all contacts
+.get((req,res, next) => {
+    // middleware
+    console.log(`Request from: ${req.originalUrl}`)
+    console.log(`Request type: ${req.method}`)
+    next();  
+}, getContacts)
 
-    // post a new contact
-    .post(addNewContact);
+// post a new contact
+.post(addNewContact);
 
-    app.route('/contact/:contactId')
-    // get specific contact
-    .get(getContactWithID)
+app.route('/contact/:contactId')
+// get specific contact
+.get(getContactWithID)
 
-    // update a contact
-    .put(updateContact)
+// update a contact
+.put(updateContact)
 
-    .delete((req, res) =>
-        res.send('DELETE request sucessful!')
-    )
+// to delete a contact
+.delete(deleteContact)
 }
 
 export default routes;
